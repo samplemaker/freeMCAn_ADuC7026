b0d0dd38be1651634b5599d2c659b6bc849b4015
samplemaker <@>
2011-10-06 02:52:39 +0200

    verbose information programming tools

--- firmware/program.sh
+++ firmware/program.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+#
+#  program.sh [firmware]
+#
+#  Program firmware with OPENOCD
+#
+#
+#  Copyright (C) 2011 samplemaker
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public License
+#  as published by the Free Software Foundation; either version 2.1
+#  of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free
+#  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA
+
 
 if [ $# -ne 1 ]
 then
@@ -7,10 +30,12 @@ then
   exit 1
 fi
 
+echo 'start openocd in terminal and wait ... '
 xterm -e "openocd -f ./openocd-usb.cfg -f ./openocd-target.cfg" &
 sleep 3
 
-VAR=$(expect -c "
+echo 'start telnet and programming ... '
+var=$(expect -c "
 spawn telnet localhost 4444
 expect \"> \"
 send \"soft_reset_halt\r\"
@@ -20,4 +45,5 @@ expect \"> \"
 send \"exit\"
 ")
 
+echo 'killall openocd ... '
 killall openocd
